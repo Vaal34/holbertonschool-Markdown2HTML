06beb5a1af14ec19614eb6d58a3972c5a42bde7a
Vaal34 <5619@holbertonstudents.com>
2023-12-23 12:51:25 +0100

    je commence a me decourager

--- markdown2html.py
+++ markdown2html.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-'''Write a script markdown2html.py that takes an argument 2 strings:
-   First argument is the name of the Markdown file.
-   Second argument is the output file name
-'''
+""" This file is made to parse some Markdown to HTML """
 
 import sys
-import os.path
-import markdown
 
 
 def main():
-
     if len(sys.argv) < 3:
-        error = "Usage: ./markdown2html.py README.md README.html"
-        print(error, file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
+    try:
+        file = open(sys.argv[1], "r")
+        html = open(sys.argv[2], "w")
+        for line in file:
+            for (idx, char) in enumerate(line):
+                if char == " " and line[idx-1] == "#":
+                    print(f"<h{idx}>{line[idx:-1]}</h{idx}>", file=html)
 
-    if not os.path.isfile(sys.argv[1]):
+        exit(0)
+    except FileNotFoundError:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         exit(1)
-
-    with open(sys.argv[1], 'r') as f:
-        text = f.read()
-        html = markdown.markdown(text)
-
-    with open(sys.argv[2], 'w') as nf:
-        nf.write(html)
-
-    exit(0)
 
 
 if __name__ == "__main__":
