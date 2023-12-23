7fe97a88c796a97935085530e75a63e041ec470f
Vaal34 <5619@holbertonstudents.com>
2023-12-23 12:18:20 +0100

    fix file not documented

--- markdown2html.py
+++ markdown2html.py
@@ -8,31 +8,39 @@ import sys
 import os.path
 import markdown
 
+
+"""
+Comment
+Comment
+Comment
+"""
+
+
+def create_markdown():
+    """
+    Function that checks if an error exists,
+    if not print nothing.
+
+    """
+
+    if len(sys.argv) < 3:
+        error = "Usage: ./markdown2html.py README.md README.html"
+        print(error, file=sys.stderr)
+        exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        exit(1)
+
+    with open(sys.argv[1], 'r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+
+    with open(sys.argv[2], 'w') as nf:
+        nf.write(html)
+
+    exit(0)
+
+
 if __name__ == '__main__':
-
-    def create_markdown():
-        """ Function that checks if an error exists, if not print nothing."""
-
-        ''' Check if the script is correctly launch '''
-        if len(sys.argv) < 3:
-            error = "Usage: ./markdown2html.py README.md README.html"
-            print(error, file=sys.stderr)
-            exit(1)
-
-        ''' Check if first arg exist '''
-        if not os.path.isfile(sys.argv[1]):
-            print(f"Missing {sys.argv[1]}", file=sys.stderr)
-            exit(1)
-
-        # Read the content of the Markdown file and convert it to HTML
-        with open(sys.argv[1], 'r') as f:
-            text = f.read()
-            html = markdown.markdown(text)
-
-        # Write the HTML content to the specified output file
-        with open(sys.argv[2], 'w') as nf:
-            nf.write(html)
-
-        exit(0)
-
     create_markdown()
