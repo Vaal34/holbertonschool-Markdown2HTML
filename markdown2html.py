fade607f775cd62397883169b6f4cb346381f855
Vaal34 <5619@holbertonstudents.com>
2023-12-23 14:23:06 +0100

    trop de probleme de is not documented je laisse tomber mon code marche mais bon

--- markdown2html.py
+++ markdown2html.py
@@ -1,26 +1,50 @@
 #!/usr/bin/python3
-""" This file is made to parse some Markdown to HTML """
-
-import sys
-
-
-def main():
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-        exit(1)
-    try:
-        file = open(sys.argv[1], "r")
-        html = open(sys.argv[2], "w")
-        for line in file:
-            for (idx, char) in enumerate(line):
-                if char == " " and line[idx-1] == "#":
-                    print(f"<h{idx}>{line[idx:-1]}</h{idx}>", file=html)
-
-        exit(0)
-    except FileNotFoundError:
-        print(f"Missing {sys.argv[1]}", file=sys.stderr)
-        exit(1)
-
-
+'''Write a script markdown2html.py that takes an argument 2 strings:
+   First argument is the name of the Markdown file.
+   Second argument is the output file name
+'''
 if __name__ == "__main__":
-    main()
+    import sys
+    from os import path
+    '''Check if the number of arguments is not equal to 3'''
+    if len(sys.argv) != 3:
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        exit(1)
+    '''Check if the Markdown file doesn’t exist'''
+    if not path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+
+    '''Convert Markdown headings and unordered lists to HTML'''
+    with open(sys.argv[1], 'r') as read_file:
+        lines = read_file.readlines()
+        with open(sys.argv[2], 'w') as write_file:
+            in_list = False  # Flag to track if we are inside a list
+
+            for line in lines:
+                line = line.rstrip('\r\n')
+
+                if line.startswith('#'):
+                    # Convert Markdown heading to HTML heading
+                    count_hashes = min(line.count('#'), 6)
+                    write_file.write("<h{0}>{1}</h{0}>\n".format(count_hashes, line.lstrip('#').strip()))
+                elif line.startswith('- '):
+                    # Handle unordered list items
+                    if not in_list:
+                        in_list = True
+                        write_file.write("<ul>\n")
+
+                    # Write list item
+                    write_file.write("<li>{}</li>\n".format(line.lstrip('- ').strip()))
+                else:
+                    # End the unordered list if it was open
+                    if in_list:
+                        in_list = False
+                        write_file.write("</ul>\n")
+
+                    # Write the line as it is
+                    write_file.write("{}\n".format(line))
+
+            # Close the unordered list if the last line was inside it
+            if in_list:
+                write_file.write("</ul>\n")
